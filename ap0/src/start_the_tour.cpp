7908caa4479555bbee7376646dff53752897715c
Despar <despargy@ece.auth.gr>
2021-02-26 11:54:15 +0200

    clear unused pack

--- ap0/src/start_the_tour.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <ros/ros.h>
-#include <math.h>
-#include "std_msgs/String.h"
-#include "nav_msgs/Odometry.h"
-#include <tf/transform_datatypes.h>
-
-
-using namespace std;
